c8c3db4555f48fc272d958bef88c51b9ad9e0bc6
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2025-03-05 12:10:49 +0100

    C# speedtests

---
+++ Cs/array_parallel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CsSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100_000_000];
+            DateTime startAt = DateTime.Now;
+            Parallel.For(0, 100_000_000, i =>
+            {
+                arr[i] = i;
+            });
+            Console.WriteLine(DateTime.Now - startAt);
+        }
+    }
+}
