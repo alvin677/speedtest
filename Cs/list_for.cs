94f0285109f0d0c1940ffeabf9df9ec32a9cfd5d
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2025-03-05 12:22:20 +0100

    Add files via upload

---
+++ Cs/list_for.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CsSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<int> arr = new System.Collections.Generic.List<int>();
+            DateTime startAt = DateTime.Now;
+            for(int i = 0; i < 100_000_000; i++)
+            {
+                arr.Add(i);
+            }
+            Console.WriteLine(DateTime.Now - startAt);
+            Console.ReadLine();
+        }
+    }
+}
