bd40417759b5a24d8d7f044cbd70c9c51bc4a3ff
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2025-03-05 12:29:29 +0100

    Add files via upload

---
+++ Cs/list_parallel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CsSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<int> arr = new System.Collections.Generic.List<int>();
+            DateTime startAt = DateTime.Now;
+            Parallel.For(0, 100_000_000, i =>
+            {
+                arr.Add(i);
+            });
+            Console.WriteLine(DateTime.Now - startAt);
+            Console.ReadLine();
+        }
+    }
+}
