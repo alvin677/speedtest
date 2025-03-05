931e00929612b814b3dab66da41e64ed445990d5
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2025-03-05 11:18:28 +0100

    C++ vector array
    This version matches the current other language speedtests

---
+++ Cpp/vector.cpp
@@ -0,0 +1,17 @@
+// CppSpeedTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+//#pragma comment(linker, "/STACK:400000024")
+int main()
+{
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    std::vector<int> array = std::vector<int>();
+    for (int i = 0; i < 100000000; i++) {
+        array.push_back(i);
+    }
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    auto d_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << d_nano << " ns\n";
+}
