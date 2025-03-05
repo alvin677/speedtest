31853148833dd2f1f63d4956793551d4ea4d5fbe
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2025-03-05 11:11:59 +0100

    Add C++ file
    Results with AMD Ryzen 5 5600X:
    Release build x86: 200 - 600 ns
    Debug build x86: 60 ms

---
+++ Cpp/array.cpp
@@ -0,0 +1,16 @@
+// CppSpeedTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <chrono>
+#pragma comment(linker, "/STACK:400000024")
+int main()
+{
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    int arr[100000000];
+    for (int i = 0; i < 100000000; i++) {
+        arr[i] = 1;
+    }
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    auto d_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << d_nano << " ns\n";
+}
